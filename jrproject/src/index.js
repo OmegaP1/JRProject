eed824962e3ee4a7408c8e79a274bfc7a3305469
OmegaP1 <j.veloso.00@gmail.com>
2022-04-16 23:05:50 +0100

    Small change in animations

--- jrproject/src/index.js
+++ jrproject/src/index.js
@@ -7,9 +7,9 @@ ReactDOM.render(
     <div class="justify-self-center">
       <div class="mb-10 md:flex my-8 max-w-[44%] mx-auto"><img class="content-center" src="images/Seta.svg"></img></div>
       <div class="grid grid-cols-3 gap-10 ml-52 mr-52 mb-32">
-        <div class="h-full"><img src="images/Lisboa.svg"></img></div>
-        <div class="h-full"><img src="images/Londres.svg"></img></div>
-        <div class="h-full"><img src="images/SaoFrancisco.svg"></img></div>
+        <div class="h-full hover:animate-spin-slow"><img src="images/Lisboa.svg"></img></div>
+        <div class="h-full hover:animate-spin-slow"><img src="images/Londres.svg"></img></div>
+        <div class="h-full hover:animate-spin-slow"><img src="images/SaoFrancisco.svg"></img></div>
       </div>
 
       <div class="bg-[#4d637ee8] cursor-pointer max-w-[6%] mx-auto hover:bg-[#506886] text-white text-center py-2 px-4 rounded-full"><a><button>ENTRAR</button></a></div>
--- jrproject/tailwind.config.js
+++ jrproject/tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'wiggle 3s linear infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%': { transform: 'rotate(0deg)' },
+          '33%': { transform: 'rotate(3deg)' },
+          '66%': { transform: 'rotate(-3deg)' },
+          '100%': { transform: 'rotate(0deg)' },
+        }
+      }
+    },
   },
   plugins: [],
 }

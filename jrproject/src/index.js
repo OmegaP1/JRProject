9773c41dfa1c92831a32f35e7ec1b71b315489dc
OmegaP1 <j.veloso.00@gmail.com>
2022-04-18 12:24:07 +0100

    React Router Working

--- jrproject/src/index.js
+++ jrproject/src/index.js
@@ -1,31 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import InitialPage from "./routes/InitialPage/initialPage.js";
+import LoginPage from "./routes/LoginPage/loginPage.js";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <div class='justify-self-center'>
-      <div class='mb-10 md:flex my-8 max-w-[44%] mx-auto'>
-        <img class='content-center' src='images/Seta.svg'></img>
-      </div>
-      <div class='grid grid-cols-3 gap-10 ml-52 mr-52 mb-32'>
-        <div class='h-full hover:animate-spin-slow'>
-          <img src='images/Lisboa.svg'></img>
-        </div>
-        <div class='h-full hover:animate-spin-slow'>
-          <img src='images/Londres.svg'></img>
-        </div>
-        <div class='h-full hover:animate-spin-slow'>
-          <img src='images/SaoFrancisco.svg'></img>
-        </div>
-      </div>
-
-      <div class='bg-button cursor-pointer max-w-[6%] mx-auto hover:bg-button_hover text-white text-center py-2 px-4 rounded-full'>
-        <a>
-          <button>ENTRAR</button>
-        </a>
-      </div>
-    </div>
-  </React.StrictMode>,
-  document.getElementById("root")
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<InitialPage />} />
+      <Route path='LoginPage' element={<LoginPage />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
---
+++ jrproject/src/routes/LoginPage/loginPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Link } from "react-router-dom";
+import "./loginPage.css";
+
+export default function LoginPage() {
+  return <h1>O bruno é gay </h1>;
+}

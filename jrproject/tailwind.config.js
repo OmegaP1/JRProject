2b62c20e18a15910d0c4bc23364535d2d1fe3f74
OmegaP1 <j.veloso.00@gmail.com>
2022-04-18 12:05:05 +0100

    Tailwind

--- jrproject/tailwind.config.js
+++ jrproject/tailwind.config.js
@@ -1,21 +1,23 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      button: "#4d637e",
+      button_hover: "#506886",
+    },
     extend: {
       animation: {
-        'spin-slow': 'wiggle 3s linear infinite',
+        "spin-slow": "wiggle 3s linear infinite",
       },
       keyframes: {
         wiggle: {
-          '0%': { transform: 'rotate(0deg)' },
-          '33%': { transform: 'rotate(3deg)' },
-          '66%': { transform: 'rotate(-3deg)' },
-          '100%': { transform: 'rotate(0deg)' },
-        }
-      }
+          "0%": { transform: "rotate(0deg)" },
+          "33%": { transform: "rotate(3deg)" },
+          "66%": { transform: "rotate(-3deg)" },
+          "100%": { transform: "rotate(0deg)" },
+        },
+      },
     },
   },
   plugins: [],
-}
+};
